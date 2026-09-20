42bf4462fdc148a9bd10cd0a54fe66f1cb1219b6
dinguezjose88-create <dinguezjose88@gmail.com>
2026-09-20 19:01:32 -0400

    nuevos ejercicios

--- src/estruturabasica/EstruturaBasica.java
+++ src/estruturabasica/EstruturaBasica.java
@@ -7,7 +7,24 @@ public class EstruturaBasica {
     
     
     public static void main(String[] args) {
-      
+        
+        
+        String  nombre ="maria";
+        int edad = 28;
+        double salario = 45000.50;
+        boolean esEmpleado = true;
+        
+        
+        double salarioAnual = salario * 12;
+        int edadProxima = edad +1;
+        
+        System.out.println("=== Informacion del Empleado ===");
+        System.out.println("nombre: " + nombre );
+        System.out.println("Edad Actual: "+ edad);
+        System.out.println(" Edad proximo ano: " + edadProxima );
+        System.out.println("salario mensual: " + salario);
+        System.out.println(" Salario anual: "+ salarioAnual);
+             System.out.println("Es empleado activo? " + esEmpleado);
     }
     
 }
